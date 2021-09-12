b3455539fce9119e2c3ad27339b993b5dece3692
rodrigodsluz <rodrigodsluz@gmail.com>
2021-09-11 21:01:05 -0300

    feat: add signin styles and status bar

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -1,12 +1,28 @@
 import React from "react";
-import { Text, View, Image } from "react-native";
+import { Text, View, Image, StatusBar } from "react-native";
 import IllustrationImg from "../../assets/illustration.png";
 import { styles } from "./styles";
 
 export function SignIn() {
   return (
     <View style={styles.container}>
-      <Image source={IllustrationImg} />
+      <StatusBar barStyle="light-content"/>
+      <Image
+        source={IllustrationImg}
+        style={styles.image}
+        resizeMode="stretch"
+      />
+
+      <View style={styles.content}>
+        <Text style={styles.title}>
+          Organize {"\n"} easily {"\n"} your gambling
+        </Text>
+
+        <Text style={styles.subtitle}>
+          Create groups to play your {"\n"}
+          favorite games with your friends
+        </Text>
+      </View>
     </View>
   );
 }
