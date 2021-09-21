ea317618c04d4871344c0db6f68fb4b40348e7e5
rodrigodsluz <rodrigodsluz@gmail.com>
2021-09-21 07:55:56 -0300

    feat: avatar component

--- src/components/Profile/index.tsx
+++ src/components/Profile/index.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { View, Text } from "react-native";
+import { Avatar } from "../Avatar";
 
 import { styles } from "./styles";
 
 export function Profile() {
   return (
     <View style={styles.container}>
+      <Avatar urlImage="https://github.com/rodrigodsluz.png"/>
       <View>
         <View style={styles.user}>
           <Text style={styles.greeting}>Olá</Text>
