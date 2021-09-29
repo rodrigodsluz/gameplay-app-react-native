3a85c92d44baff56dc0b14e65d233306f0c1c032
rodrigodsluz <rodrigodsluz@gmail.com>
2021-09-29 05:18:17 -0300

    feat: add member component

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { ImageBackground, Text, View } from "react-native";
+import { ImageBackground, Text, View, FlatList } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { Fontisto } from "@expo/vector-icons";
-
 import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 import BannerImg from "../../assets/banner.png";
+import { ListHeader } from "../../components/ListHeader";
+import { Member } from "../../components/Member";
 
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
+import { ListDivider } from "../../components/ListDivider";
 
 export function AppointmentDetails() {
+  const members = [
+    {
+      id: "1",
+      username: "Rodrigo",
+      avatar_url: "http://github.com/rodrigodsluz.png",
+      status: "online",
+    },
+    {
+      id: "2",
+      username: "Rodrigo",
+      avatar_url: "http://github.com/rodrigodsluz.png",
+      status: "offline",
+    },
+  ];
   return (
     <Background>
       <Header
@@ -32,6 +48,16 @@ export function AppointmentDetails() {
           </Text>
         </View>
       </ImageBackground>
+
+      <ListHeader title="Players" subtitle="Total 3" />
+
+      <FlatList
+        data={members}
+        keyExtractor={(item) => item.id}
+        renderItem={({ item }) => <Member data={item} />}
+        ItemSeparatorComponent={() => <ListDivider />}
+        style={styles.members}
+      />
     </Background>
   );
 }
