931565c02d31fa6d3fcea3cccf2b85f28c96aa16
rodrigodsluz <rodrigodsluz@gmail.com>
2021-10-12 05:55:55 -0300

    feat: start signin auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -1,4 +1,14 @@
 import React, { createContext, ReactNode, useContext, useState } from "react";
+import * as AuthSession from "expo-auth-session";
+import {
+  REDIRECT_URI,
+  SCOPE,
+  RESPONSE_TYPE,
+  CLIENT_ID,
+  CDN_IMAGE,
+} from "../configs";
+import { api } from '../services/api';
+
 
 type User = {
   id: string;
@@ -11,6 +21,7 @@ type User = {
 
 type AuthContextData = {
   user: User;
+  signIn: () => Promise<void>;
 };
 
 type AuthProviderProps = {
@@ -21,9 +32,23 @@ export const AuthContext = createContext({} as AuthContextData);
 
 const AuthProvider = ({ children }: AuthProviderProps) => {
   const [user, setUser] = useState<User>({} as User);
+  const [loading, setLoading] = useState(false);
+
+  const signIn = async () => {
+    try {
+      setLoading(true);
+
+      const authUrl = `${api.defaults.baseURL}/oauth2/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`;
+      console.log(authUrl)
+
+      AuthSession.startAsync({ authUrl });
+    } catch (error) {
+      throw new Error("Not possible to authenticate");
+    }
+  };
 
   return (
-    <AuthContext.Provider value={{ user }}>{children}</AuthContext.Provider>
+    <AuthContext.Provider value={{ user, signIn }}>{children}</AuthContext.Provider>
   );
 };
 
