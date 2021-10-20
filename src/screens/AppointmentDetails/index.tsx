e772117e786cf969443e74a6c030e4819671ef0e
rodrigodsluz <rodrigodsluz@gmail.com>
2021-10-20 15:54:24 -0300

    feat: add fetch guild widget function

--- src/screens/AppointmentDetails/index.tsx
+++ src/screens/AppointmentDetails/index.tsx
@@ -1,5 +1,5 @@
-import React from "react";
-import { ImageBackground, Text, View, FlatList } from "react-native";
+import React, { useState, useEffect } from "react";
+import { ImageBackground, Text, View, FlatList, Alert } from "react-native";
 import { BorderlessButton } from "react-native-gesture-handler";
 import { useRoute } from "@react-navigation/core";
 
@@ -8,36 +8,50 @@ import { Background } from "../../components/Background";
 import { Header } from "../../components/Header";
 import BannerImg from "../../assets/banner.png";
 import { ListHeader } from "../../components/ListHeader";
-import { Member } from "../../components/Member";
+import { Member, MemberProps } from "../../components/Member";
 
 import { styles } from "./styles";
 import { theme } from "../../global/styles/theme";
 import { ListDivider } from "../../components/ListDivider";
 import { ButtonIcon } from "../../components/ButtonIcon";
-import { AppointmentProps } from '../../components/Appointment';
+import { AppointmentProps } from "../../components/Appointment";
+import { api } from "../../services/api";
+import { Load } from "../../components/Load";
 
 type Params = {
   guildSelected: AppointmentProps;
 };
 
+type GuildWidget = {
+  id: string;
+  name: string;
+  instant_invite: string;
+  members: MemberProps[];
+};
+
 export function AppointmentDetails() {
-  const members = [
-    {
-      id: "1",
-      username: "Rodrigo",
-      avatar_url: "http://github.com/rodrigodsluz.png",
-      status: "online",
-    },
-    {
-      id: "2",
-      username: "Rodrigo",
-      avatar_url: "http://github.com/rodrigodsluz.png",
-      status: "offline",
-    },
-  ];
+  const [widget, setWidget] = useState<GuildWidget>({} as GuildWidget);
+  const [loading, setLoading] = useState(true);
 
   const route = useRoute();
   const { guildSelected } = route.params as Params;
+
+  async function fetchGuildWidget() {
+    try {
+      const response = await api.get(
+        `/guilds/${guildSelected.guild.id}/widget.json`
+      );
+      setWidget(response.data);
+    } catch {
+      Alert.alert("Check server settings. Is the Widget enabled?");
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  useEffect(() => {
+    fetchGuildWidget();
+  }, []);
 
   return (
     <Background>
@@ -52,23 +66,30 @@ export function AppointmentDetails() {
 
       <ImageBackground source={BannerImg} style={styles.banner}>
         <View style={styles.bannerContent}>
-          <Text style={styles.title}> { guildSelected.guild.name }</Text>
+          <Text style={styles.title}> {guildSelected.guild.name}</Text>
 
-          <Text style={styles.subtitle}>
-          { guildSelected.description }
-          </Text>
+          <Text style={styles.subtitle}>{guildSelected.description}</Text>
         </View>
       </ImageBackground>
 
-      <ListHeader title="Players" subtitle="Total 3" />
+      {loading ? (
+        <Load />
+      ) : (
+        <>
+          <ListHeader
+            title="Jogadores"
+            subtitle={`Total ${widget.members.length}`}
+          />
 
-      <FlatList
-        data={members}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => <Member data={item} />}
-        ItemSeparatorComponent={() => <ListDivider isCentered />}
-        style={styles.members}
-      />
+          <FlatList
+            data={widget.members}
+            keyExtractor={(item) => item.id}
+            renderItem={({ item }) => <Member data={item} />}
+            ItemSeparatorComponent={() => <ListDivider isCentered />}
+            style={styles.members}
+          />
+        </>
+      )}
 
       <View style={styles.footer}>
         <ButtonIcon title="Join the match" />
